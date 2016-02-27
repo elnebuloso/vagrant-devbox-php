af6a1c34bbe98b390a14d85babfcdcf509d708b4
elnebuloso <jeff.tunessen@gmail.com>
2016-02-27 02:39:28 +0100

    update for 1.9.0

---
+++ files/docker/provision/ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+################################################################################################
+
+if [ ! -f "$(which ansible)" ]; then
+    apt-get -y install software-properties-common
+    apt-add-repository -y ppa:ansible/ansible
+    apt-get update
+    apt-get -y install ansible
+fi;
+
+################################################################################################
