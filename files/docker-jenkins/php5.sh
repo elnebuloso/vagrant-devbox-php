954bce4eaa1b53b9c7c63d136b4ac0a53f943638
elnebuloso <jeff.tunessen@gmail.com>
2016-02-27 02:23:24 +0100

    update for 1.9.0

--- files/docker-jenkins/php5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-################################################################################################
-
-apt-get update
-apt-get install -y php5-cli php5-curl php5-geoip php5-imagick php5-mcrypt php5-memcached php5-mongo php5-mysql php5-sqlite php5-xdebug php5-xsl
-
-################################################################################################
