740e0b71dc718cdc2727c1aa4f16164179a50d33
elnebuloso <jeff.tunessen@gmail.com>
2016-05-28 16:43:02 +0200

    - todos

--- ansible/roles/docker-mysql/templates/my.tpl
+++
@@ -1,3 +0,0 @@
-[mysqld]
-bind-address = 0.0.0.0
-sql_mode = STRICT_ALL_TABLES
--- provision/003-ansible-galaxy.sh
+++ provision/003-ansible-galaxy.sh
@@ -2,10 +2,11 @@
 
 ################################################################################################
 
-ansible-galaxy install --force elnebuloso.server-base,1.1.0
-ansible-galaxy install --force elnebuloso.apache2,1.1.0
-ansible-galaxy install --force elnebuloso.php56,1.1.0
-ansible-galaxy install --force elnebuloso.php56-apache2,1.1.2
-ansible-galaxy install --force elnebuloso.docker,1.1.0
+ansible-galaxy install elnebuloso.server-base,1.1.0
+ansible-galaxy install elnebuloso.apache2,1.1.0
+ansible-galaxy install elnebuloso.php56,1.1.0
+ansible-galaxy install elnebuloso.php56-apache2,1.1.2
+ansible-galaxy install --force elnebuloso.docker,1.2.0
+ansible-galaxy install --force elnebuloso.docker-mysql,1.0.0
 
 ################################################################################################
