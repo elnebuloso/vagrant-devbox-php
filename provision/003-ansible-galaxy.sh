43ecf94e5a7346daeb405556667dfc197acb4b5d
elnebuloso <jeff.tunessen@gmail.com>
2016-05-28 17:17:28 +0200

    - todos

--- provision/003-ansible-galaxy.sh
+++ provision/003-ansible-galaxy.sh
@@ -10,6 +10,6 @@ ansible-galaxy install elnebuloso.php56,1.1.0
 ansible-galaxy install elnebuloso.php56-apache2,1.1.2
 ansible-galaxy install elnebuloso.docker,1.2.0
 ansible-galaxy install elnebuloso.docker-mysql,1.0.0
-ansible-galaxy install elnebuloso.ci-jenkins
+ansible-galaxy install elnebuloso.ci-jenkins,1.0.0
 
 ################################################################################################
