049f3a6bb72bcc61a11ec4d5e0f7686c8fd3d4aa
elnebuloso <jeff.tunessen@gmail.com>
2016-06-03 01:50:50 +0200

    - todos

--- provision/003-ansible-galaxy.sh
+++ provision/003-ansible-galaxy.sh
@@ -8,6 +8,7 @@ ansible-galaxy install --force elnebuloso.docker,1.2.1
 ansible-galaxy install --force elnebuloso.docker-mysql,1.2.0
 ansible-galaxy install --force elnebuloso.ci,1.0.0
 ansible-galaxy install --force elnebuloso.ci-vcs,1.0.0
+ansible-galaxy install --force elnebuloso.ci-compass,1.0.0
 ansible-galaxy install --force elnebuloso.ci-jenkins,1.2.0
 
 # vendor roles
