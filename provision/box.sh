5981b2ce661164285c307335510a40d651a189af
Administrator <jan.thoennessen@gmail.com>
2017-04-22 03:58:28 +0200

    release

--- provision/box.sh
+++ provision/box.sh
@@ -4,6 +4,6 @@
 
 sudo rm -rf /etc/ansible/roles/*
 sudo ansible-galaxy install -r /vagrant/ansible/.box-roles.yml
-sudo ansible-playbook -i 'localhost,' -c local /vagrant/ansible/.box.yml
+sudo ansible-playbook -i 'localhost,' -c local /vagrant/ansible/.box.yml $@
 
 ################################################################################################
