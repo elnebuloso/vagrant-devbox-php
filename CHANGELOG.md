c2fdb9f4c6821cdc43ab1d548b43a366cd32b42d
elnebuloso <FAq3alRWUIpValLqUENi>
2018-01-07 19:12:58 +0100

    ## [4.3.0] - 2018-01-07 ### Changed - fixed puttykeygen

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 
+## [4.3.0] - 2018-01-07
+### Changed
+- fixed puttykeygen
+
+
 ## [4.2.0] - 2017-12-10
 ### Changed
 - vagrant config setting vm_private_ip
--- provision/001-keys.sh
+++ provision/001-keys.sh
@@ -13,14 +13,14 @@ fi;
 if [ ! -f "/vagrant/.ssh/root/id_rsa" ]; then
     mkdir -p /vagrant/.ssh/root
     ssh-keygen -f "/vagrant/.ssh/root/id_rsa" -P ""
-    puttygen "/vagrant/.ssh/root/id_rsa" -O private -o "/vagrant/.ssh/root/id_rsa.ppk"
+    puttygen /vagrant/.ssh/root/id_rsa -O private -o /vagrant/.ssh/root/id_rsa.ppk -C "vagrant-devbox-php"
 fi;
 
 rm -rf /root/.ssh
 mkdir -p /root/.ssh
 cp -r /vagrant/.ssh/root/* /root/.ssh
 
-find /vagrant/.ssh/authorized_keys/ -type f -name '*.pub' -exec cat {} \; -exec echo "" \; > /root/.ssh/authorized_keys
+find /vagrant/.ssh/ -type f -name '*.pub' -exec cat {} \; -exec echo "" \; > /root/.ssh/authorized_keys
 
 chmod 700 /root/.ssh
 chmod 600 /root/.ssh/*
